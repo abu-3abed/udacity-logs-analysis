7124a67c626b2fa93976c45040c626d8a3ac738a
abu-3abed <abu.3abed.2010@gmail.com>
2018-10-27 20:25:22 +0300

    reformat code in pep8

--- news.py
+++ news.py
@@ -13,7 +13,9 @@ def main():
     print("1. What are the most popular three articles of all time?")
 
     first_cursor.execute(
-        "SELECT articles.title, COUNT(*) FROM articles JOIN log ON log.path = CONCAT('/article/', articles.slug) WHERE log.status = '200 OK' GROUP BY articles.title ORDER BY COUNT(*) DESC LIMIT 3;")
+        """SELECT articles.title, COUNT(*) FROM articles JOIN log ON log.path =
+        CONCAT('/article/', articles.slug) WHERE log.status = '200 OK' GROUP BY
+        articles.title ORDER BY COUNT(*) DESC LIMIT 3;""")
     result1 = first_cursor.fetchall()
 
     print()
@@ -27,7 +29,11 @@ def main():
     print("2. Who are the most popular article authors of all time?")
     print()
 
-    second_cursor.execute("SELECT authors.name, COUNT(*) FROM articles JOIN log ON log.path = CONCAT('/article/', articles.slug) JOIN authors ON authors.id = articles.author WHERE log.status = '200 OK' GROUP BY authors.name ORDER BY COUNT(*) DESC;")
+    second_cursor.execute("""SELECT authors.name, COUNT(*) FROM articles JOIN
+    log ON log.path = CONCAT('/article/', articles.slug) JOIN authors ON
+    authors.id = articles.author WHERE log.status = '200 OK' GROUP BY
+    authors.name ORDER BY COUNT(*) DESC;""")
+
     result2 = second_cursor.fetchall()
 
     print()
@@ -39,12 +45,18 @@ def main():
     print()
 
     """ Third query """
-    """note: third query sometimes gives me end of transaction block error but closing the connection to
-		DB and connecting to it again seems to make it magically works."""
+    """note: third query sometimes gives me end of transaction block error but
+    closing the connection to DB and connecting to it again seems to make it
+    magically works."""
     print("3. On which days did more than 1% of requests lead to errors?")
     print()
 
-    third_cursor.execute("""SELECT t1.day, (TRUNC(CAST(t2.count::FLOAT / t1.count * 100 AS numeric),2)) AS "% error" FROM (SELECT TO_CHAR(time, 'FMMonth DD, YYYY') AS day, COUNT(*) FROM log GROUP BY day) AS t1 JOIN (SELECT TO_CHAR(time, 'FMMonth DD, YYYY') AS day, COUNT(*) FROM log WHERE status != '200 OK' GROUP BY day ORDER BY day) AS t2 ON t1.day = t2.day ORDER BY "% error" DESC LIMIT 1;""")
+    third_cursor.execute("""SELECT t1.day, (TRUNC(CAST(t2.count::FLOAT /
+    t1.count * 100 AS numeric),2)) AS "% error" FROM (SELECT TO_CHAR(time,
+    'FMMonth DD, YYYY') AS day, COUNT(*) FROM log GROUP BY day) AS t1 JOIN
+    (SELECT TO_CHAR(time, 'FMMonth DD, YYYY') AS day, COUNT(*) FROM log WHERE
+    status != '200 OK' GROUP BY day ORDER BY day) AS t2 ON t1.day = t2.day
+    ORDER BY "% error" DESC LIMIT 1;""")
     result3 = third_cursor.fetchall()
 
     print()
@@ -54,11 +66,6 @@ def main():
         print("- " + row[0] + " -- " + str(row[1]) + "% errors.")
     print()
     print()
-
-
-"""SELECT articles.title, COUNT(*) FROM articles JOIN log ON log.path = CONCAT('/article/', articles.slug) WHERE log.status = '200 OK' GROUP BY articles.title ORDER BY COUNT(*) DESC LIMIT 3;
-SELECT authors.name, COUNT(*) FROM articles JOIN log ON log.path = CONCAT('/article/', articles.slug) JOIN authors ON authors.id = articles.author WHERE log.status = '200 OK' GROUP BY authors.name ORDER BY COUNT(*) DESC;
-SELECT t1.day, (TRUNC(CAST(t2.count::FLOAT / t1.count * 100 AS numeric),2)) AS "% error" FROM (SELECT TO_CHAR(time, 'FMMonth DD, YYYY') AS day, COUNT(*) FROM log GROUP BY day) AS t1 JOIN (SELECT TO_CHAR(time, 'FMMonth DD, YYYY') AS day, COUNT(*) FROM log WHERE status != '200 OK' GROUP BY day ORDER BY day) AS t2 ON t1.day = t2.day ORDER BY "% error" DESC LIMIT 1;"""
 
 if __name__ == '__main__':
     main()
